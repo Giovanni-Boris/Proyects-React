bcfcf7b4a56f8f30b7c5e5d606b0c296855dc9cc
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-01 13:08:47 -0500

    Defining folder strucutura and styled components

---
+++ proyect1-website/src/globalStyles.js
@@ -0,0 +1,19 @@
+import styled,{ createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+	*{
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+		font-family: 'Monserrat', sans-serif;
+	}
+`;
+
+export const Container = styled.div``;
+export const MainHeading = styled.h1``;
+export const Heading = styled.h2``;
+export const TextWrapper = styled.span``;
+export const Section = styled.section``;
+export const Row = styled.div``;
+export const Column = styled.div``;
+export const Button = styled.button``;
