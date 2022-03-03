e38824d38e05cd14ee1852f1eec20deb5d3474d6
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-02 21:01:09 -0500

    Add components and their length height and width to make a responsive website

---
+++ portfolio/src/components/Intro/Intro.js
@@ -0,0 +1,10 @@
+import './intro.scss'
+const Intro = () => {
+	return (
+		<div className="intro">
+			dsdsdsdsdsd
+		</div>
+	)
+}
+
+export default Intro
---
+++ portfolio/src/components/Portfolio/Portfolio.js
@@ -0,0 +1,10 @@
+import './portfolio.scss';
+const Portfolio = () => {
+	return (
+		<div className="portfolio">
+			sadsdsd	
+		</div>
+	)
+}
+
+export default Portfolio
