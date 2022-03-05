427f6c5e724e431c26671ced70766114f0217b17
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-05 17:09:10 -0500

    I created the route for update the users

--- chatRealTime/server/routes/users.js
+++ chatRealTime/server/routes/users.js
@@ -1,19 +1,37 @@
 const router = require("express").Router();
 const User = require("../models/User");
-
-router.post("/register", async(req,res)=>{
-	const newUser = new User({
-		username: req.body.username,
-		email: req.body.email,
-		password: req.body.password
-	});
-
-	try{
-		const user = await newUser.save();
-		res.status(200).json(user);
-	} catch (err){
-		console.log(err);
+const bcrypt  = require("bcrypt");
+//update user
+router.put("/:id", async(req,res)=>{
+	if(req.body.userId === req.params.id || req.body.isAdmin){
+		if(req.body.password){
+			try{
+				const salt = await bcrypt.genSalt(10);
+				req.body.password = await bcrypt.hash(req.body.password, salt);
+			}catch(err){
+				console.log("contra");
+				return res.status(500).json(err);
+			}
+		}
+		try{
+			console.log(req.body.password);
+			const user = await User.findByIdAndUpdate(req.params.id,{
+				$set: req.body,
+			});
+			res.status(200).json({message:"Account has been updated"});
+		} catch(err){
+			return res.status(500).json(err);
+		}
+	} else{
+		return res.status(403).json({message:"You can update only your account"})
 	}
-;})
+})
+//delete user
+
+//get a user
+
+//follow a user
+
+//unfollow a user
 
 module.exports = router;
