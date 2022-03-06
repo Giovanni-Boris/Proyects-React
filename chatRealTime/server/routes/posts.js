8d683a3d40e5e609ed6e51123aab2e1bb5a1cf67
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-06 05:29:20 -0500

    route to update a post

--- chatRealTime/server/routes/posts.js
+++ chatRealTime/server/routes/posts.js
@@ -12,10 +12,36 @@ router.post("/", async(req,res)=>{
 		res.status(500).json(err);
 	}
 });
-//updata a post
+//update a post
 
+router.put("/:id", async(req,res)=>{
+	try{
+		const post = await Post.findById(req.params.id);
+		if(post.userId === req.body.userId){
+			await post.updateOne({$set:req.body})
+			res.status(200).json({message:"your post has been updated"});
+		}else{
+			res.status(403).json({message:"you can update only your post"});
+		}
+	}catch(err){
+		res.status(500).json(err);
+	}
+})
 //like a post
 
+router.delete("/:id", async(req,res)=>{
+	try{
+		const post = await Post.findById(req.params.id);
+		if(post.userId === req.body.userId){
+			await post.deleteOne()
+			res.status(200).json({message:"the post has been deleted"});
+		} else {
+			res.status(403).json({message:"you can delete only your post"});
+		}
+	}catch(err){
+		res.status(500).json(err);
+	}
+})
 
 //get a post
 
