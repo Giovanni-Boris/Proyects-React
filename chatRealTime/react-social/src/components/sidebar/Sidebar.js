93e33dbcbf8b1546bdee4bbce737d518ce5025cb
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-06 19:23:39 -0500

    component sidebar has been made, only stay any updates but it will make more later

--- chatRealTime/react-social/src/components/sidebar/Sidebar.js
+++ chatRealTime/react-social/src/components/sidebar/Sidebar.js
@@ -1,8 +1,67 @@
 import "./sidebar.css";
+import RssFeedIcon from '@mui/icons-material/RssFeed';
+import ChatIcon from '@mui/icons-material/Chat';
+import PlayCircleFilledOutlinedIcon from '@mui/icons-material/PlayCircleFilledOutlined';
+import GroupIcon from '@mui/icons-material/Group';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import EventIcon from '@mui/icons-material/Event';
+import SchoolIcon from '@mui/icons-material/School';
+import SidebarListItem from './SidebarListItem'
+
+const listOptions = [
+	{icon:RssFeedIcon, name:"Feed"},
+	{icon:ChatIcon, name:"Chats"},
+	{icon:PlayCircleFilledOutlinedIcon, name:"Videos"},
+	{icon:GroupIcon, name:"Groups"},
+	{icon:BookmarkIcon, name:"Bookmarks"},
+	{icon:HelpOutlineIcon, name:"Questions"},
+	{icon:WorkOutlineIcon, name:"Jobs"},
+	{icon:EventIcon, name:"Events"},
+	{icon:SchoolIcon, name:"Courses"}
+];
+
 const Sidebar = () => {
+	//si pones padding  a side bar se desfigurara la caja flexible usa un wrapper para hacerlo mejor
 	return (
-		<div className="sidebar"s >
-			sidebar
+		<div className="sidebar">
+
+			<div className="sidebarWrapper">
+				<ul className="sidebarList">
+					{listOptions.map((el,index)=>(
+						<SidebarListItem key={el.name} Icon={el.icon} options={el.name}/>
+					))}
+				</ul>
+				<button className="sidebarButton">Show More</button>
+				<hr className="sidebarHr"/>
+				<ul className="sidebarFriendList">
+					<li className="sidebarFriend">
+						<img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
+						<span className="sidebarFriendName">Jane Doe</span>
+					</li>
+					<li className="sidebarFriend">
+						<img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
+						<span className="sidebarFriendName">Jane Doe</span>
+					</li>
+					<li className="sidebarFriend">
+						<img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
+						<span className="sidebarFriendName">Jane Doe</span>
+					</li>
+					<li className="sidebarFriend">
+						<img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
+						<span className="sidebarFriendName">Jane Doe</span>
+					</li>
+					<li className="sidebarFriend">
+						<img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
+						<span className="sidebarFriendName">Jane Doe</span>
+					</li>
+					<li className="sidebarFriend">
+						<img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
+						<span className="sidebarFriendName">Jane Doe</span>
+					</li>
+				</ul>
+			</div>
 		</div>
 	)
 }
---
+++ chatRealTime/react-social/src/components/sidebar/SidebarListItem.js
@@ -0,0 +1,12 @@
+
+const SidebarListItem = ({Icon, options}) => {
+	return (
+		<li className="sidebarListItem">
+          	<Icon className="sidebarIcon"/>
+            <span className="sidebarListItemText">{options}</span>
+        </li>
+		
+	)
+}
+
+export default SidebarListItem
