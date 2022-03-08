8a948880e4fad844d7e97e9526f5231aba893dbc
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-07 19:36:18 -0500

    Add a link on the component navbar

--- chatRealTime/react-social/src/components/navbar/Navbar.js
+++ chatRealTime/react-social/src/components/navbar/Navbar.js
@@ -3,12 +3,14 @@ import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
 import ChatIcon from '@mui/icons-material/Chat';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-
+import {Link} from 'react-router-dom';
 const Navbar = () => {
 	return (
 		<div className="navbarContainer">
 			<div className="navbarLeft">
-				<span className="logo">LamaSocial</span>	
+				<Link to="/" style={{textDecoration: "none"}}>
+					<span className="logo">LamaSocial</span>	
+				</Link>
 			</div>
 			<div className="navbarCenter">
 				<div className="searchbar">
