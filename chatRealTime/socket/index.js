4d88a7e97ea4bfc8927a79d730b5eab8528337b8
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-11 06:18:31 -0500

    get users from socket server

--- chatRealTime/socket/index.js
+++ chatRealTime/socket/index.js
@@ -1,9 +1,21 @@
-const io = require("socket.io",{
+const io = require("socket.io")(8900,{
 	cors: {
 		origin: "http://localhost:3000",
 	},
 });
 
+let users = [];
+
+const addUser = (userId, socketId) => {
+	!users.some((user) => user.userId === userId) &&
+		users.push({ userId, socketId});
+}
 io.on("connection",(socket)=>{
 	console.log(" a user connected. ");
+	//io.emit("welcome","hello this is socket server");
+	//take userId and socketId from user
+	socket.on("addUser",(userId)=>{
+		addUser(userId,socket.id);
+		io.emit("getUsers", users)
+	})
 })
