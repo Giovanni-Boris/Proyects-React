8d72524286ce11ca80462e98bbec9260ce34617e
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-11 11:03:20 -0500

    chat real time has been made

--- chatRealTime/socket/index.js
+++ chatRealTime/socket/index.js
@@ -31,7 +31,9 @@ io.on("connection",(socket)=>{
 	// send and get message
 	
 	socket.on("sendMessage",({ senderId, receiverId, text})=>{
+		console.log(senderId,receiverId,text);
 		const user = getUser(receiverId);
+		console.log(user);
 		io.to(user.socketId).emit("getMessage", {
 			senderId,
 			text,
