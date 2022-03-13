e47e7f400582a3ac8207dfd47610e32afa776d6a
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-13 11:19:19 -0500

    making sidebar component

--- blog-website/react-blog/src/components/sidebar/Sidebar.js
+++ blog-website/react-blog/src/components/sidebar/Sidebar.js
@@ -6,7 +6,6 @@ const Sidebar = () => {
 			<div className="sidebarItem">
 				<span className="sidebarTitle">ABOUT ME</span>
 				<img 
-					className=""
           src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
 					alt=""
 				/>
