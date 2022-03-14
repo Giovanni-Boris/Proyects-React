c917f78af0fce14a0a39cfaa33fed0871f872e37
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-13 19:39:28 -0500

    login page has been made

--- blog-website/react-blog/src/App.js
+++ blog-website/react-blog/src/App.js
@@ -1,13 +1,14 @@
 import Topbar from "./components/topbar/Topbar"
 import Home from "./pages/home/Home"
-import Setting from "./pages/settings/Settings"
+import Settings from "./pages/settings/Settings"
 import Single from "./pages/single/Single"
 import Write from "./pages/write/Write"
+import Login from "./pages/login/Login"
 function App() {
   return (
     <>
       <Topbar/>  
-      <Setting/>
+      <Login/>
     </>
   );
 }
