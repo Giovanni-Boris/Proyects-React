435326ea5c91408045eafba1e4f78ac9f0e3cf4a
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-14 14:00:21 -0500

    multer implemnted

--- blog-website/server/index.js
+++ blog-website/server/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts")
 const categoryRoute = require("./routes/categories")
+
 dotenv.config();
 app.use(express.json())
 
@@ -16,7 +18,20 @@ mongoose.connect(process.env.MONGO_URL,{
 	.then(console.log("Connected to BD"))
 	.catch((err) => console.log(err));
 
+//IMG
+const storage = multer.diskStorage({
+	destination:(req,file,cb)=>{
+		cb(null,"images")
+	},
+	filename:(req,file,cb)=>{
+		cb(null,"hello.jpeg")
+	}
+})
 
+const upload = multer({storage})
+app.post("/api/upload", upload.single("file"),(req,res)=>{
+	res.status(200).json("File has been uploaded")
+})
 //Routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
