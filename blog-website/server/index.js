458cd44fd0f042f625c4ffb348dd60dd9f9d04b6
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-14 11:48:05 -0500

    update user

--- blog-website/server/index.js
+++ blog-website/server/index.js
@@ -3,9 +3,10 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
-
+const userRoute = require("./routes/users")
 dotenv.config();
 app.use(express.json())
+
 mongoose.connect(process.env.MONGO_URL,{
 	useNewUrlParser: true, 
  	useUnifiedTopology: true,
@@ -16,7 +17,7 @@ mongoose.connect(process.env.MONGO_URL,{
 
 //Routes
 app.use("/api/auth", authRoute);
-
+app.use("/api/users", userRoute);
 
 app.listen("5000", ()=>{
 	console.log("Backend is running.");
