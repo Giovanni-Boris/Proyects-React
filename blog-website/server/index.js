55b063940ff162dd8d974780c54428f9e46bcb0a
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-14 07:29:10 -0500

    makin models and routes folder

--- blog-website/server/index.js
+++ blog-website/server/index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL,{
+	useNewUrlParser: true, 
+ 	useUnifiedTopology: true,
+})
+	.then(console.log("Connected to BD"))
+	.catch((err) => console.log(err))
+;
 
 app.listen("5000", ()=>{
 	console.log("Backend is running.");
