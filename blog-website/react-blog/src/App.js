51cdb8fc5da7fa2fcfb26e20c39ef4c058d4c518
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-13 20:20:59 -0500

    Routes inside our app.js

--- blog-website/react-blog/src/App.js
+++ blog-website/react-blog/src/App.js
@@ -5,12 +5,21 @@ import Single from "./pages/single/Single"
 import Write from "./pages/write/Write"
 import Login from "./pages/login/Login"
 import Register from "./pages/register/Register"
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
 function App() {
+  const user = false;
   return (
-    <>
-      <Topbar/>  
-      <Register/>
-    </>
+    <Router>
+      <Topbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/register' element={user ? <Home/> : <Register/>}/>
+        <Route path='/login' element={user ? <Home/> : <Login/>}/>
+        <Route path='/write' element={user ? <Write/> : <Home/>}/>
+        <Route path='/settings' element={user ? <Settings/> : <Home/>}/>
+        <Route path='/post/:postId' element={<Single/>}/>
+      </Routes>
+    </Router>
   );
 }
 
