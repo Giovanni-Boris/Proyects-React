f13ff3425ab1386b2197811af843126ccf1664d6
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-15 07:24:02 -0500

    login with context provider

--- blog-website/react-blog/src/context/AuthContext.js
+++ blog-website/react-blog/src/context/AuthContext.js
@@ -1,16 +1,20 @@
-import { createContext, useReducer} from "react";
+import { createContext, useReducer, useEffect} from "react";
 import {authReducer} from "../reducers/authReducer";
 
 const AuthContext = createContext();
 
 const INITIAL_STATE = {
-	user:null,
+	user:JSON.parse(localStorage.getItem("user")) || null,
 	isFetching:false,
 	error:false,
 }
 
 const AuthProvider = ({children}) => {
 	const [state, dispatch] = useReducer(authReducer, INITIAL_STATE)
+	useEffect(() => {
+		console.log("hola")
+		localStorage.setItem("user",JSON.stringify(state.user));
+	}, [state.user])
 	const data = {
 		user: state.user,
 		isFetching: state.isFetching,
--- blog-website/react-blog/src/pages/login/Login.js
+++ blog-website/react-blog/src/pages/login/Login.js
@@ -1,15 +1,46 @@
 import "./login.css"
 import { Link } from 'react-router-dom'
+import {useRef, useContext} from "react";
+import AuthContext from "../../context/AuthContext"
+import {TYPES} from "../../actions/authActions";
+import axios from "axios";
 const Login = () => {
+	const userRef = useRef();
+	const passwordRef = useRef();
+	const { dispatch, isFetching} = useContext(AuthContext)
+	const handleSubmit = async(e) => {
+		e.preventDefault();
+		dispatch({type:TYPES.LOGIN_START})
+		try{
+			const res = await axios.post("/auth/login",{
+				username: userRef.current.value,
+				password: passwordRef.current.value,
+			})
+			dispatch({ type:TYPES.LOGIN_SUCCESS, payload:res.data })
+		} catch(err){
+			console.log(err);
+			dispatch({ type: TYPES.LOGIN_FAILURE});
+		}
+	}
 	return (
 		<div className="login">
 			<span className="loginTitle">Login</span>
-			<form className="loginForm">
-				<label>Email:</label>
-				<input type="email" className="loginInput" placeholder="Enter your email..."/>
+			<form className="loginForm" onSubmit={handleSubmit}>
+				<label>Username:</label>
+				<input 
+					type="text" 
+					className="loginInput" 
+					placeholder="Enter your username..."
+					ref={userRef}
+				/>
 				<label>Password:</label>
-				<input type="password" className="loginInput" placeholder="Enter your password..."/>
-				<button className="loginButton">Login</button>
+				<input
+					type="password" 
+					className="loginInput" 
+					placeholder="Enter your password..."
+					ref={passwordRef}
+				/>
+				<button className="loginButton" type="submit" disabled={isFetching}>Login</button>
 			</form>
 			<button className="loginRegisterButton">
 				<Link className="link" to="/register">Register</Link>	
