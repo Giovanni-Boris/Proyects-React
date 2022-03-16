94e5c9aff186c4e78140e59573e20b3b2fbfc972
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-16 15:19:35 -0500

    component sider created aor something like that

---
+++ e-commerce/e-commerce-react/src/data.js
@@ -0,0 +1,42 @@
+
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://us.123rf.com/450wm/vgstudio/vgstudio1702/vgstudio170200055/70986742-muy-feliz-hermosa-mujer-joven-en-ropa-casual-con-bolsas-de-la-compra-aislado-sobre-fondo-blanco-mode.jpg?ver=6",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://media.vogue.mx/photos/5c073298202e78c0f3327abb/4:3/w_1199,h_899,c_limit/street_style_paris_fashion_week_primavera_verano_2018_975634327.jpg",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://casaydiseno.com/wp-content/uploads/2019/10/ropa-moda-mujeres-40-ideas.jpg",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
