965cb41b25007e71991ffc40087987f2482142e1
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-17 10:54:33 -0500

    login page created

--- e-commerce/e-commerce-react/src/App.js
+++ e-commerce/e-commerce-react/src/App.js
@@ -2,10 +2,11 @@ import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <Login/>
     </div>
   );
 }
