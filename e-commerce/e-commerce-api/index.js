5e2780ced683148c5640760c9f857785bd1ed14b
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-18 11:26:13 -0500

    register authroute

--- e-commerce/e-commerce-api/index.js
+++ e-commerce/e-commerce-api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user')
+const authRoute = require("./routes/auth");
 dotenv.config();
 
 const option = { useNewUrlParser: true, useUnifiedTopology: true}
@@ -12,9 +13,11 @@ mongoose.connect(process.env.MONGO_URL,option)
 	.catch(error => console.log('Error db: ', error))
 
 app.use(express.json())
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
 
 app.listen(process.env.PORT || 5000, () =>{
 	console.log("Backend server is running");
 });
+
