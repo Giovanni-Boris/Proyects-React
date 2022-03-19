e365371310aabae699a31d292d2fb7cd1b61054c
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-18 21:10:56 -0500

    order route created

--- e-commerce/e-commerce-api/routes/cart.js
+++ e-commerce/e-commerce-api/routes/cart.js
@@ -18,7 +18,7 @@ router.post("/", verifyToken, async(req,res)=>{
 
 router.put("/:id",verifyTokenAndAuthorization ,async(req,res)=>{
 	try{
-		const updatedCart = await User.findByIdAndUpdate(req.params.id,{
+		const updatedCart = await Cart.findByIdAndUpdate(req.params.id,{
 			$set: req.body
 		}, {new:true});
 
