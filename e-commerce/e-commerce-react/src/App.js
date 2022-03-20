2cb9faa245c340f09cd72ac9ea9ddaec275f0c57
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-19 19:07:14 -0500

    routes for our apges with react-router-dom

--- e-commerce/e-commerce-react/src/App.js
+++ e-commerce/e-commerce-react/src/App.js
@@ -4,11 +4,20 @@ import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 function App() {
+  const user = true;
   return (
-    <div className="App">
-      <Cart/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/products/:category" element={<ProductList/>}/>
+        <Route path="/product/:id" element={<Product/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={user ? <Navigate to="/"/> : <Login/>}/>
+        <Route path="/register" element={user ? <Navigate to="/"/> : <Register/>}/>
+      </Routes>
+    </Router>
   );
 }
 
