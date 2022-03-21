b5a1de18b90b5f0e7f6f86f8e2d234ac512f8b73
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-21 14:48:59 -0500

    widgetSm created componenet

--- e-commerce/admin/src/pages/home/Home.js
+++ e-commerce/admin/src/pages/home/Home.js
@@ -2,11 +2,17 @@ import FeaturedInfo from "../../components/featureInfo/FeaturedInfo";
 import Chart from "../../components/chart/Chart";
 import "./home.css";
 import {userData} from "../../dummyData";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
 const Home = () => {
 	return (
 		<div className="home">
 			<FeaturedInfo/>
 			<Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+			<div className="homeWidgets">
+				<WidgetSm/>
+				<WidgetLg/>
+			</div>
 		</div>
 	)
 }
