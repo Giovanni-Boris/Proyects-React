39a7618f4f889c7c446185e9909ecf51e671ad12
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-22 16:44:15 -0500

    WidgetLg wight ang sm with hooks

--- e-commerce/admin/src/pages/login/Login.js
+++ e-commerce/admin/src/pages/login/Login.js
@@ -1,10 +1,16 @@
-import "./login.css"
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {login} from "../../redux/apiCalls";
+import {useNavigate} from "react-router-dom";
 const initialForm = ({
 	username:"",
 	password:"",
 })
 const Login = () => {
+	let navigate = useNavigate()
 	const [form, setForm] = useState(initialForm);
+	const dispatch = useDispatch()
+
 	const handleChange = (e) => {
 		let {value,name} = e.target;
 		setForm({
@@ -14,23 +20,35 @@ const Login = () => {
 	}
 	const handleClick = (e) => {
 		e.preventDefault();
-		//
+		login(dispatch,{...form})
+		navigate("/dashboard/")
 	}
 	return (
-		<div>
-			<input 
+		<div style={{
+		        height: "100vh",
+		        display: "flex",
+		        flexDirection: "column",
+		        alignItems: "center",
+		        justifyContent: "center",
+		      }}
+		>
+			<input
+				style={{ padding: 10, marginBottom: 20 }}
 				name="username" 
 				type="text" 
 				placeholder="username"
 				onChange={handleChange}
 			/>
 			<input 
+				style={{ padding: 10, marginBottom: 20 }}
 				name="password" 
 				type="password" 
 				placeholder="password"
 				onChange={handleChange}
 			/>
-			<button onClick={handleClick}>Login</button>
+			<button onClick={handleClick} style={{ padding: 10, width:100 }}>
+				Login
+			</button>
 			
 		</div>
 	)
