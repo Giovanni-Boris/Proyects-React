c231ebe5aad44023919f52d948334aba64fff553
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-23 13:38:05 -0500

    settings

--- e-commerce/e-commerce-react/src/requestMethods.js
+++ e-commerce/e-commerce-react/src/requestMethods.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-// const TOKEN =
-//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-//     .accessToken || "";
 
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMzRlODhmMjE3ODNjZGQ1OTRhMmE0NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0Nzc5NjIyOCwiZXhwIjoxNjQ4MDU1NDI4fQ.R2xuPPcGGPPlPQYLLAfzmuXW8uyxUIukoY9Zl5tIwMc";
+
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
