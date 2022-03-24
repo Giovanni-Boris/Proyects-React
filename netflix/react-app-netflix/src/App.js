c90994833deb42c42e595b1b55136957df86a27a
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-24 16:19:36 -0500

    login page

--- netflix/react-app-netflix/src/App.js
+++ netflix/react-app-netflix/src/App.js
@@ -1,11 +1,12 @@
-import "./app.scss"
-import Home from "./pages/home/Home"
-import Watch from "./pages/watch/Watch"
-import Register from "./pages/register/Register"
+import "./app.scss";
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <Login/>
     </div>
   );
 }
