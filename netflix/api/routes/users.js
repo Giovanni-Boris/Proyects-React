ba196cd96e3c7c501086e43cd1249e8108a5d29a
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-25 11:29:37 -0500

    stats user get user an get all users

--- netflix/api/routes/users.js
+++ netflix/api/routes/users.js
@@ -29,7 +29,7 @@ router.put("/:id",verifyToken, async(req,res)=>{
 	} else {
 		res.status(403).json("You can update only your account!"); 
 	}
-})
+});
 //DELETE
 router.delete("/:id",verifyToken, async(req,res)=>{
 	if(req.user.id === req.params.id || req.user.isAdmin){
@@ -42,13 +42,76 @@ router.delete("/:id",verifyToken, async(req,res)=>{
 	} else {
 		res.status(403).json("You can delete only your account!"); 
 	}
-})
+});
 //GET
 
+router.get("/find/:id", async(req,res)=>{
+	try{
+		const user = await User.findById(req.params.id)
+		const { password, ...info} = user._doc;
+		res.status(200).json(info)
+	}catch(err){
+		res.status(500).json(err);
+	}
+})
 
 //GET ALL
-
+router.get("/",verifyToken, async(req,res)=>{
+	const query = req.query.new;
+	if(req.user.isAdmin){
+		try{
+			const users = query ? await User.find().sort({_id:-1}).limit(5) : await User.find();
+			res.status(200).json(users)
+		}catch(err){
+			res.status(500).json(err);
+		}
+	} else {
+		res.status(403).json("You are not allowed to see all users!"); 
+	}
+})
 //GET USER STATS
+
+router.get("/stats", async(req,res)=>{
+	const today = new Date();
+	const lastYear = today.setFullYear(today.setFullYear() - 1);
+	console.log(lastYear);
+	try{
+		const data = await User.aggregate([
+			{
+				$project:{
+					month: { $month: "$createdAt"}
+				},
+
+			},
+			{
+				$group: {
+					_id: "$month",
+					total: {$sum:1}
+				},
+			},
+		]);
+		res.status(200).json(data);
+	}catch(err){
+		console.log(err)
+		res.status(500).json(err);
+	}
+})
 
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
