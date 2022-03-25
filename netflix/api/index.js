a057a6c615d887e118aec74752b7e42ffacfb63d
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-25 06:19:31 -0500

    model user created

--- netflix/api/index.js
+++ netflix/api/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const option = { useNewUrlParser: true, useUnifiedTopology: true}
+mongoose.connect(process.env.MONGO_URL,option)
+	.then(()=> console.log('Base de datos conectada'))
+	.catch(error => console.log('Error db: ', error))
 
 
+
+//midlewares
+app.use(morgan("common")) 
 
 app.listen(8800, ()=>{
 	console.log("Backend server is running!");
