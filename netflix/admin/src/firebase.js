912aab5112e85c7b9492892c6bb9e9ccb6df703e
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-27 19:01:41 -0500

    configuration of firebase

---
+++ netflix/admin/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3U5eXalesrzMrO4Q5Wvy9B7buCSrW2QU",
+  authDomain: "netflix-27882.firebaseapp.com",
+  projectId: "netflix-27882",
+  storageBucket: "netflix-27882.appspot.com",
+  messagingSenderId: "496856021500",
+  appId: "1:496856021500:web:6826361fbe1f70a88bda79",
+  measurementId: "G-VWL6BEZFQ4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+export default storage;
