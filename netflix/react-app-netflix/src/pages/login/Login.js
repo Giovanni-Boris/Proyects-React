05db0ef43591a7e25b51ef6ef5057e7d0eed77b0
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-28 11:14:03 -0500

    login page updates

--- netflix/react-app-netflix/src/pages/login/Login.js
+++ netflix/react-app-netflix/src/pages/login/Login.js
@@ -1,7 +1,23 @@
+import { login } from "../../context/authContext/apiCalls";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import { useState, useContext } from "react";
 import "./login.scss"
 
 const Login = () => {
+	const [form, setForm] = useState();
+	const { dispatch } = useContext(AuthContext);
 
+	const handleChange = (e) => {
+		let { value, name } = e.target;
+		setForm({
+			...form,
+			[name]:value,
+		})
+	}
+	const handleLogin = (e) => {
+		e.preventDefault();
+		login(form, dispatch)
+	}
 	return (
 		<div className="login">
 			<div className="top">
@@ -16,9 +32,9 @@ const Login = () => {
 			<div className="container">
 	       		<form>
 	       			<h1>Sign In</h1>
-	       			<input type="email" placeholder="Email or phone number"/>
-	       			<input type="password" placeholder="Password"/>
-	       			<button className="loginButton">Sign In</button>
+	       			<input type="email" name="email" placeholder="Email or phone number" onChange={handleChange}/>
+	       			<input type="password" name="password" placeholder="Password" onChange={handleChange}/>
+	       			<button className="loginButton" onClick={handleLogin}>Sign In</button>
 	       			<span>
 			            New to Netflix? <b>Sign up now.</b>
 			        </span>
