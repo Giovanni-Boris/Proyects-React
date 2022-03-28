fa5d07bd69ec9ac2e0771b35fe4a76edea988d70
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-03-28 10:09:59 -0500

    update users

--- netflix/admin/src/context/listContext/listReducer.js
+++ netflix/admin/src/context/listContext/listReducer.js
@@ -37,26 +37,26 @@ const listReducer =(state,action)=>{
 				isFetching: false,
 				error: true,
 			}
-		/*case "UPDATE_MOVIE_START":
+		case "UPDATE_LIST_START":
 			return{
 				...state,
 				isFetching: true,
 				error: false,
 			}
-		case "UPDATE_MOVIE_SUCCESS":
+		case "UPDATE_LIST_SUCCESS":
 			return{
-				movies: state.movies.map(
-					(movie) => movie._id === action.payload._id ? action.payload : movie
+				lists: state.lists.map(
+					(list) => list._id === action.payload._id ? action.payload : list
 				),
 				isFetching: false,
 				error: false,
 			}
-		case "UPDATE_MOVIE_FAILURE":
+		case "UPDATE_LIST_FAILURE":
 			return{
 				...state,
 				isFetching: false,
 				error: true,
-			}*/
+			}
 		case "DELETE_LIST_START":
 			return{
 				...state,
